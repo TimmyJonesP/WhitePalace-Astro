cfd52acd47edbb06d9d6e8569c8921bf84ff4c10
Sandro <sandrocarioli43@gmail.com>
2024-04-25 15:57:21 -0300

    e

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://TimmyJonesP.github.io",
-  base: "White Palace",
   integrations: [tailwind()],
 });
